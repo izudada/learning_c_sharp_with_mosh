38fda7a896c9b21a3979e6d6e34b815e40d45560
Anthony <tonyudeagbala@gmail.com>
2021-11-19 06:06:21 +0000

    Added notes on conversions

--- Program.cs
+++ Program.cs
@@ -34,8 +34,94 @@ namespace EFS
             // Console.WriteLine(debug);
 
             // String formatting while checking limits of some data types
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            // Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            // Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            //  Constants (You are not allowed to change value of constants nor do a reassignment)
+            const float Pi = 3.14f;
+
+            // Type Conversions
+            // - Implicit type conversion
+            // - Explicit type conversion (casting)
+            // - Conversion between non-compatoble types
+
+
+            // - Implicit type conversion
+
+            /*
+                Implicit - Indirect
+
+                A byte contains 1 byte of memory, but an int takes 4bytes. 
+                By copying the byte into an int below, we make an
+                implicit converion.
+
+                Note: There is no possibility of data loss in implicit conversion,
+                        because there is an increase in the size of the new data type (int)
+            */
+            byte b = 1;
+            int i = b; 
+
+
+            // - Explicit type conversion (casting)
+
+            /* The below conversion will only be possible using casting "(byte)"
+                The compiler knows there will be data loss, so a directive need
+                be given to tell it to go ahead with the conversion.
+            */
+
+            int firstVar = 1;
+            byte secondVar = (byte)firstVar;
+            // Console.WriteLine(secondVar);
+
+
+            //  Non Compatible Types/Conversion
+
+            /*
+                Explicit conversion won't work for non compatible 
+                conversions. Thus we can use the convert class
+                or the Parse method.
+            */
+            string thirdNumber = "10";
+            int castNumber = Convert.ToInt32(thirdNumber);
+            // or
+            int castNumber2 = int.Parse(thirdNumber); 
+
+
+            /*
+                Using try and catch for overflows
+                 in noncompatible conversions
+            */
+
+            try
+            {
+                var n = "1234";
+                byte secondNum = Convert.ToByte(n);
+                // Console.WriteLine(secondNum);
+            }
+            catch (System.Exception)
+            {
+                
+                // Console.WriteLine("This convertion did not happen. Probably an overflow happened.");
+            }
+
+
+            /*
+                Converting strings to boolean with non compatibility conversion
+            */
+
+            try
+            {
+                 string data = "false";
+                 bool data2 = Convert.ToBoolean(data);
+                 Console.WriteLine(data2);
+            
+            }
+            catch (System.Exception)
+            {
+                
+                Console.WriteLine("This convertion did not happen.");
+            }
+
         }
     }
 }
